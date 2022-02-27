69569b91b1c97ee655cd160baaa3905037ed79c4
Govind-S-B <b.s.dnivog@gmail.com>
2022-02-28 03:45:15 +0530

    Morse code generating function and display functionality

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,34 @@ class MorseCodeMain extends StatefulWidget {
 
 class _MorseCodeMainState extends State<MorseCodeMain> {
 
-  String Morse = ". . . _ _ _ . . . __ . . . . ..";
+  String Morse = "";
   TextEditingController textController = TextEditingController();
+
+  Map<String, String> MorseMap = { 'A':'. - ', 'B':'- . . . ',
+    'C':'- . - . ', 'D':'- . . ', 'E':'. ',
+    'F':'. . - . ', 'G':'- - . ', 'H':'. . . . ',
+    'I':'. . ', 'J':'. - - - ', 'K':'- . - ',
+    'L':'. - . . ', 'M':'- - ', 'N':'- . ',
+    'O':'- - - ', 'P':'. - - . ', 'Q':'- - . - ',
+    'R':'. - . ', 'S':'. . . ', 'T':'- ',
+    'U':'. . - ', 'V':'. . . - ', 'W':'. - - ',
+    'X':'- . . - ', 'Y':'- . - - ', 'Z':'- - . . ',
+    '1':'. - - - - ', '2':'. . - - - ', '3':'. . . - - ',
+    '4':'. . . . - ', '5':'. . . . . ', '6':'- . . . . ',
+    '7':'- - . . . ', '8':'- - - . . ', '9':'- - - - . ',
+    '0':'- - - - - ', ', ':'- - . . - - ', '.':'. - . - . - ',
+    '?':'. . - - . . ', '/':'- . . - . ', '-':'- . . . . - ',
+    '(':'- . - - . ', ')':'- . - - . - '," ":"       "};
+
+  String morse_parser(String text_to_convert){
+    String morse = "";
+    for (var x = 0; x < text_to_convert.length ;++x){
+      print(text_to_convert[x]);
+      morse += MorseMap[text_to_convert[x]]!;
+      morse += "   ";
+    }
+    return morse;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,19 +65,27 @@ class _MorseCodeMainState extends State<MorseCodeMain> {
         child: Column(
           children: [
             SizedBox(height: 30,),
-            Text(Morse,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 60
-            ),),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(Morse,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 40
+              ),),
+            ),
             SizedBox(height: 30,),
             Padding(
               padding: const EdgeInsets.all(10.0),
               child: Center(
                 child:  TextField(
                   controller: textController,
+                  onSubmitted: (value){
+                    setState(() {
+                      Morse = morse_parser(value.toUpperCase()); // the text from controller must be passed to a morse parser function and then to a flash function
+                    });
+                  },
                   cursorColor: Colors.black,
                   style: TextStyle(
                       color: Color(0xff654ea3)
@@ -61,7 +95,7 @@ class _MorseCodeMainState extends State<MorseCodeMain> {
                       icon: Icon(Icons.send),
                       onPressed: (){
                         setState(() {
-                          Morse = textController.text; // the text from controller must be passed to a morse parser function and then to a flash function
+                          Morse = morse_parser(textController.text.toUpperCase()); // the text from controller must be passed to a morse parser function and then to a flash function
                         });
                       },
                     ),
