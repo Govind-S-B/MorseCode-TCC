1c0f3da8bbba24d0c7bda195efb242ebdf458d2b
Govind-S-B <b.s.dnivog@gmail.com>
2022-02-27 21:52:48 +0530

    Primitive UI : base elements placed , WIP - extracting input

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,70 @@ class MorseCodeMain extends StatefulWidget {
 }
 
 class _MorseCodeMainState extends State<MorseCodeMain> {
+
+  String Morse = ". . . _ _ _ . . . __ . . . . ..";
+  TextEditingController textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('MorseCode TCC'),
+        backgroundColor: Color(0xff654ea3),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.topLeft,
+              colors: [
+                Color(0xff654ea3),
+                Color(0xffeaafc8),
+              ],
+            )
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: 30,),
+            Text(Morse,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 60
+            ),),
+            SizedBox(height: 30,),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Center(
+                child:  TextField(
+                  controller: textController,
+                  cursorColor: Colors.black,
+                  style: TextStyle(
+                      color: Color(0xff654ea3)
+                  ),
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: (){
+                        setState(() {
+                          Morse = textController.text; // the text from controller must be passed to a morse parser function and then to a flash function
+                        });
+                      },
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
